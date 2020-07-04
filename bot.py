8807ebb9cae6218275001b54a655a29c5d8d9b29
Karto4an <33024982+Karto4an@users.noreply.github.com>
2020-07-04 15:08:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ def callback_InlineKeyboard(call):
 		print(repr(e))
 
 @bot.pre_checkout_query_handler(func=lambda query: True)
-async def process_pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery):
+def process_pre_checkout_query(pre_checkout_query: types.PreCheckoutQuery):
 	bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True)
 	bot.send_message(
 		message.chat.id,
