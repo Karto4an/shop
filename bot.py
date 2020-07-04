aa31368956ea4bc090c37b99fabea7d3bb2d6965
Karto4an <33024982+Karto4an@users.noreply.github.com>
2020-07-04 18:42:29 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import telebot
+import price
 from telebot import types
 from dotenv import load_dotenv
 from aiogram.types.message import ContentType
@@ -16,13 +17,36 @@ name_keyboard_choise_1 = "windows"
 name_keyboard_choise_2 = "office"
 name_inlinekeyboard_versionchoise_1 = "Home"
 name_inlinekeyboard_versionchoise_2 = "Profesional"
-name_inlinekeyboard_callback_1 = "windows_home"
-name_inlinekeyboard_callback_2 = "windows_pro"
+name_inlinekeyboard_callback_1 = "invoice_windowshome"
+name_inlinekeyboard_callback_2 = "invoice_windowspro"
 name_inlinekeyboard_versionchoise_3 = "Home"
 name_inlinekeyboard_versionchoise_4 = "Profesional"
-name_inlinekeyboard_callback_3 = "office_home"
+name_inlinekeyboard_callback_3 = "invoice_officehome"
 name_inlinekeyboard_callback_4 = "invoice_officepro"
 
+price_1_title = "Microsoft Windows 10 Home"
+price_1_description = "Version of Windows 10 for home usage"
+price_1_currency = "uah"
+
+price_1 = types.LabeledPrice(label = "MS Windows Home", amount = 100)
+
+price_2_title = "Microsoft Windows 10 Profesional"
+price_2_description = "Version of Windows 10 for profesional usage"
+price_2_currency = "uah"
+
+price_2 = types.LabeledPrice(label = "MS Windows Pro", amount = 140)
+
+price_3_title = "Microsoft Office Home"
+price_3_description = "Version of MS Office for home usage"
+price_3_currency = "uah"
+
+price_3 = types.LabeledPrice(label = "MS Office Home", amount = 80)
+
+price_4_title = "Microsoft Office Profesional"
+price_4_description = "Version of MS Office for profesional usage"
+price_4_currency = "uah"
+
+price_4 = types.LabeledPrice(label = "MS Office Pro", amount = 120)
 #--------------------------------------------------------------
 
 welcome_keyboard = types.ReplyKeyboardMarkup(resize_keyboard = True)
@@ -40,13 +64,13 @@ office_versionselect_keyboard_home = types.InlineKeyboardButton(name_inlinekeybo
 office_versionselect_keyboard_pro = types.InlineKeyboardButton(name_inlinekeyboard_versionchoise_4, callback_data = name_inlinekeyboard_callback_4)
 office_versionselect_keyboard.add(office_versionselect_keyboard_home, office_versionselect_keyboard_pro)
 
-price_1 = types.LabeledPrice(label='Настоящая Машина Времени', amount=4200000)
 
-@bot.message_handler(commands=['start'])
+
+@bot.message_handler(commands = ["start"])
 def welcome(message):
 	bot.send_message(message.chat.id, "Hello, chose product please", reply_markup = welcome_keyboard)
 
-@bot.message_handler(content_types=["text"])
+@bot.message_handler(content_types = ["text"])
 def choise(message):
 	if message.chat.type == "private":
 		if message.text == "Windows":
@@ -60,28 +84,65 @@ def choise(message):
 @bot.callback_query_handler(func = lambda call: True)
 def callback_InlineKeyboard(call):
 	try:
-		if call.data == "windows_home":
-			bot.send_message(call.message.chat.id, "Home")
-		elif call.data == "windows_pro":
-			bot.send_message(call.message.chat.id, "Pro")
-		elif call.data == "office_home":
-			bot.send_message(call.message.chat.id, "Home")
-		elif call.data == "office_pro":
-			bot.send_message(call.message.chat.id, "Pro")
-		elif call.data == "invoice_officepro":
+		if call.data == name_inlinekeyboard_callback_1:
 			bot.send_invoice(
 				call.message.chat.id,
-				title = "tm_title",
-				description = "tm_description",
+				title = price_1_title,
+				description = price_1_description,
 				provider_token = provider_payment_token,
-				currency = "uah",
+				currency = price_1_currency,
 				photo_url = "photo.jfif",
 				photo_height = 800,
 				photo_width = 600,
 				photo_size =750,
 				is_flexible = False,
-				prices = [price_1],
-				start_parameter = 'time-machine-example',
+				prices = [price_4],
+				start_parameter = "time-machine-example",
+				invoice_payload = 1)
+		elif call.data == name_inlinekeyboard_callback_2:
+			bot.send_invoice(
+				call.message.chat.id,
+				title = price_2_title,
+				description = price_2_description,
+				provider_token = provider_payment_token,
+				currency = price_2_currency,
+				photo_url = "photo.jfif",
+				photo_height = 800,
+				photo_width = 600,
+				photo_size =750,
+				is_flexible = False,
+				prices = [price_4],
+				start_parameter = "time-machine-example",
+				invoice_payload = 1)
+		elif call.data == name_inlinekeyboard_callback_3:
+			bot.send_invoice(
+				call.message.chat.id,
+				title = price_3_title,
+				description = price_3_description,
+				provider_token = provider_payment_token,
+				currency = price_3_currency,
+				photo_url = "photo.jfif",
+				photo_height = 800,
+				photo_width = 600,
+				photo_size =750,
+				is_flexible = False,
+				prices = [price_4],
+				start_parameter = "time-machine-example",
+				invoice_payload = 1)
+		elif call.data == name_inlinekeyboard_callback_4:
+			bot.send_invoice(
+				call.message.chat.id,
+				title = price_4_title,
+				description = price_4_description,
+				provider_token = provider_payment_token,
+				currency = price_4_currency,
+				photo_url = "photo.jfif",
+				photo_height = 800,
+				photo_width = 600,
+				photo_size =750,
+				is_flexible = False,
+				prices = [price_4],
+				start_parameter = "time-machine-example",
 				invoice_payload = 1)
 
 	except Exception as e:
